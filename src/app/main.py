5cd442f5c6fcbc5f1b7a70d245e192f5c8021757
Matheus <matheusmlf@gmail.com>
2022-08-02 00:35:43 -0300

    Routes

--- src/app/main.py
+++ src/app/main.py
@@ -1,8 +1,16 @@
+from app.api import ping
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/ping")
-def pong():
-    return {"ping": "pong"}
+app.include_router(ping.router)
+
+
+# To do - Routes
+
+# /notes/ - GET - READ - GET ALL NOTES
+# /notes/:id/ - GET - READ - GET A SINGLE NOTE
+# /notes/ - POST - CREATE - ADD A NOTE
+# /notes/:id/ - PUT - UPDATE - UPDATE A NOTE
+# /notes/:id/ - DELETE - DELETE - DELETE A NOTE
