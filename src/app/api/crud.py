152583d428ca56717230be63a058a5475ac4a496
Matheus <matheusmlf@gmail.com>
2022-08-02 08:30:54 -0300

    GET case

--- src/app/api/crud.py
+++ src/app/api/crud.py
@@ -7,3 +7,8 @@ from app.db import notes, database
 async def post(payload: NoteSchema):
     query = notes.insert().values(title=payload.title, description=payload.description)
     return await database.execute(query=query)
+
+
+async def get(id: int):
+    query = notes.select().where(title=id == notes.c.id)
+    return await database.fetch_onde(query=query)
